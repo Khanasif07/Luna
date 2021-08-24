08e5343f7d174ae5174df4db219758c2c46ab5fb
Sohrab Ansari <sohrab.ansari@appinventiv.com>
2021-08-24 20:03:37 +0530

    plot image on xAxis charts

--- Luna/Xib's/PostLogin/BottomSheetChartCell.swift
+++ Luna/Xib's/PostLogin/BottomSheetChartCell.swift
@@ -43,6 +43,12 @@ class BottomSheetChartCell: UITableViewCell,ChartViewDelegate {
             marker.chartView = chartView
             marker.minimumSize = CGSize(width: 50.0, height: 30.0)
             chartView.marker = marker
+            
+            let customXAxisRender = XAxisCustomRenderer(viewPortHandler: self.chartView.viewPortHandler,
+                                                        xAxis: chartView.xAxis,
+                                                        transformer: self.chartView.getTransformer(forAxis: .left),
+                                                        cgmData: self.cgmData)
+            self.chartView.xAxisRenderer = customXAxisRender
             chartView.data = data
         }
     }
@@ -114,7 +120,7 @@ class BottomSheetChartCell: UITableViewCell,ChartViewDelegate {
         let values = cgmData.map { (data) -> ChartDataEntry in
             return ChartDataEntry(x: Double(data.date), y: Double(data.sgv), icon: #imageLiteral(resourceName: "reservoir7Bars"))
         }
-
+        
         let set1 = LineChartDataSet(entries: values, label: "")
         set1.drawIconsEnabled = true
         setup(set1)
@@ -141,29 +147,168 @@ class BottomSheetChartCell: UITableViewCell,ChartViewDelegate {
             dataSet.formLineWidth = 1
             dataSet.formSize = 15
     }
-
     
-//    public  func setUpChartData(){
-//        let series = ChartSeries(data: data)
-//        series.area = true
-//        series.color = #colorLiteral(red: 0.2705882353, green: 0.7843137255, blue: 0.5803921569, alpha: 1)
-//        chartView.lineWidth = 3.0
-//        chartView.xLabelsOrientation = .horizontal
-////        chartView.xLabels =  xRangeValue
-//        chartView.yLabels = [0, 50, 100, 150, 200, 250]
-//        //
-//        chartView.showYLabelsAndGrid = true
-//        chartView.showXLabelsAndGrid = true
-//        chartView.axesColor = .clear
-//       //
-////        chartView.xLabelsFormatter = { String(Int(roundf(Float($1)))) + " am" }
-//        chartView.yLabelsFormatter = { String(Int(roundf(Float($1)))) + "" }
-//        chartView.add(series)
-//        chartView.reloadInputViews()
-//    }
+    
+    //    public  func setUpChartData(){
+    //        let series = ChartSeries(data: data)
+    //        series.area = true
+    //        series.color = #colorLiteral(red: 0.2705882353, green: 0.7843137255, blue: 0.5803921569, alpha: 1)
+    //        chartView.lineWidth = 3.0
+    //        chartView.xLabelsOrientation = .horizontal
+    ////        chartView.xLabels =  xRangeValue
+    //        chartView.yLabels = [0, 50, 100, 150, 200, 250]
+    //        //
+    //        chartView.showYLabelsAndGrid = true
+    //        chartView.showXLabelsAndGrid = true
+    //        chartView.axesColor = .clear
+    //       //
+    ////        chartView.xLabelsFormatter = { String(Int(roundf(Float($1)))) + " am" }
+    //        chartView.yLabelsFormatter = { String(Int(roundf(Float($1)))) + "" }
+    //        chartView.add(series)
+    //        chartView.reloadInputViews()
+    //    }
     
     // MARK: - IBActions
     //===========================
     
     
+}
+
+
+class XAxisRendererWithImage: XAxisRenderer {
+    
+    var cgmData : [ShareGlucoseData] = []
+    
+    override func drawLabel(context: CGContext, formattedLabel: String, x: CGFloat, y: CGFloat,
+                            attributes: [NSAttributedString.Key: Any],constrainedToSize: CGSize,
+                            anchor: CGPoint, angleRadians: CGFloat) {
+        
+        super.drawLabel(context: context,
+                        formattedLabel: formattedLabel,
+                        x: x, y: y,
+                        attributes: attributes,
+                        constrainedToSize: constrainedToSize,
+                        anchor: anchor, angleRadians: angleRadians)
+        
+        guard let xAxis = self.axis as? XAxis else { return }
+        let entries = xAxis.entries
+        print(entries)
+        
+        print(x,y)
+        print(anchor)
+        
+        context.beginPath()
+        let image = #imageLiteral(resourceName: "reservoir9Bars")
+        context.draw(image.cgImage!, in: CGRect(x: x, y: y - 20, width: 15, height: 15))
+        context.strokePath()
+    }
+}
+
+public class XAxisCustomRenderer: XAxisRenderer {
+    
+    var cgmData : [ShareGlucoseData] = []
+    
+    init(viewPortHandler: ViewPortHandler, xAxis: XAxis, transformer: Transformer, cgmData: [ShareGlucoseData]) {
+        self.cgmData = cgmData
+        super.init(viewPortHandler: viewPortHandler, xAxis: xAxis, transformer: transformer)
+    }
+    
+    override public func drawLabels(context: CGContext, pos: CGFloat, anchor: CGPoint){
+        guard
+            let xAxis = self.axis as? XAxis,
+            let transformer = self.transformer
+        else { return }
+        
+        #if os(OSX)
+        let paraStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        #else
+        let paraStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        #endif
+        paraStyle.alignment = .center
+        
+        let labelAttrs: [NSAttributedString.Key : Any] = [NSAttributedString.Key.font: xAxis.labelFont,
+                                                          NSAttributedString.Key.foregroundColor: xAxis.labelTextColor,
+                                                          NSAttributedString.Key.paragraphStyle: paraStyle]
+        let FDEG2RAD = CGFloat(Double.pi / 180.0)
+        
+        let labelRotationAngleRadians = xAxis.labelRotationAngle * FDEG2RAD
+        
+        let centeringEnabled = xAxis.isCenterAxisLabelsEnabled
+        
+        let valueToPixelMatrix = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        var labelMaxSize = CGSize()
+        
+        if xAxis.isWordWrapEnabled{
+            labelMaxSize.width = xAxis.wordWrapWidthPercent * valueToPixelMatrix.a
+        }
+        
+        let entries = xAxis.entries
+        
+        for i in stride(from: 0, to: entries.count, by: 1){
+            if centeringEnabled{
+                position.x = CGFloat(xAxis.centeredEntries[i])
+            }
+            else{
+                position.x = CGFloat(entries[i])
+            }
+            
+            position.y = 0.0
+            position = position.applying(valueToPixelMatrix)
+            
+            if viewPortHandler.isInBoundsX(position.x){
+                let label = xAxis.valueFormatter?.stringForValue(xAxis.entries[i], axis: xAxis) ?? ""
+                
+                let labelns = label as NSString
+                
+                if xAxis.isAvoidFirstLastClippingEnabled{
+                    // avoid clipping of the last
+                    if i == xAxis.entryCount - 1 && xAxis.entryCount > 1{
+                        let width = labelns.boundingRect(with: labelMaxSize,
+                                                         options: .usesLineFragmentOrigin,
+                                                         attributes: labelAttrs, context: nil).size.width
+                        
+                        if width > (viewPortHandler.offsetRight) * 2.0
+                            && position.x + width > viewPortHandler.chartWidth
+                        {
+                            position.x -= width / 2.0
+                        }
+                    }
+                    else if i == 0{ // avoid clipping of the first
+                        let width = labelns.boundingRect(with: labelMaxSize,
+                                                         options: .usesLineFragmentOrigin,
+                                                         attributes: labelAttrs, context: nil).size.width
+                        position.x += width / 2.0
+                    }
+                }
+                
+                let rawIcon: UIImage = #imageLiteral(resourceName: "reservoir7Bars")
+                let icon: CGImage = rawIcon.cgImage!
+                
+                
+                //Draw the time labels
+                drawLabel(
+                    context: context,
+                    formattedLabel: label,
+                    x: position.x,
+                    y: position.y,
+                    attributes: labelAttrs,
+                    constrainedToSize: labelMaxSize,
+                    anchor: anchor,
+                    angleRadians: labelRotationAngleRadians)
+                
+                //let indexData = cgmData[Int(i)]
+                
+                
+                //Based on Condition Display Image.....
+                if label == "08 am" ||  label == "03 pm" || label == "12 pm"{
+                    context.draw(icon, in: CGRect(x: position.x - 10 , y: position.y - 20, width: CGFloat(15), height: CGFloat(15)))
+                }else{
+                    context.draw(icon, in: CGRect(x: position.x, y: position.y - 20, width: CGFloat(0), height: CGFloat(0)))
+                }
+            }
+        }
+    }
 }
