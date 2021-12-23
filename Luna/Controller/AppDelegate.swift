6318b4fdd88b197c28c0ba7e78a5de659f11a524
Asif Khan <asif.khan@appinventiv.com>
2021-12-23 13:43:13 +0530

    # Offline server data

--- Luna/Controller/AppDelegate.swift
+++ Luna/Controller/AppDelegate.swift
@@ -141,8 +141,14 @@ extension AppDelegate {
 //        #endif
         if let options = FirebaseOptions(contentsOfFile: filePath) {
             FirebaseApp.configure(options: options)
+            Database.database().isPersistenceEnabled = true
+            let all = Database.database().reference(withPath: ApiKey.sessionData)
+            all.keepSynced(true)
         } else {
             FirebaseApp.configure()
+            Database.database().isPersistenceEnabled = true
+            let all = Database.database().reference(withPath:  ApiKey.sessionData)
+            all.keepSynced(true)
         }
     }
 }
